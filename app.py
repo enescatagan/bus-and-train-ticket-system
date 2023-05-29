dd1eaf2acf51cc5d949fe47b289a945b96563798
Taha Ugurcan Ongel <tahagurcanoffical@gmail.com>
2023-05-29 18:05:50 +0300

    app.py bus tickets

--- app.py
+++ app.py
@@ -1,4 +1,10 @@
 from flask import Flask, render_template
+from bus import Bus
+from train import Train
+bus_tickets = []
+for bus_ticket in bus_tickets:
+    bus_tick_obj = Bus(bus_ticket["id"], bus_ticket["company"], bus_ticket["departure"], bus_ticket["destination"], bus_ticket["date"], bus_ticket["time"], bus_ticket["fee"])
+
 app = Flask(__name__)
 
 
@@ -13,7 +19,7 @@ def otobus():
 
 @app.route('/otobus_biletleri')
 def otobus_biletleri():
-    return render_template("otobus_biletleri.html")
+    return render_template("otobus_biletleri.html", bus_tickets=bus_tickets)
 
 
 @app.route('/tren')
