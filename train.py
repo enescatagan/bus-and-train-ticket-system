b35ee7f030f1f9a17ca38ee0651cc15abd99a6a2
enescatagan <enescatagan@gmail.com>
2023-05-29 17:41:58 +0300

    bus class update

--- train.py
+++
@@ -1,8 +0,0 @@
-class Bus():
-    def __init__(self, name, departure, destination, date, time) -> None:
-      self.name = name
-      self.departure = departure  
-      self.destination = destination
-      self.date = date
-      self.time = time
-        
