9b5ba284a7f5758005b7d9027c419fe7336afd78
enescatagan <enescatagan@gmail.com>
2023-05-29 19:29:44 +0300

    Train compartmens and seats json data

--- app.py
+++ app.py
@@ -10,10 +10,11 @@ for bus_ticket in bus_json:
     bus_tick_obj = Bus(bus_ticket["id"], bus_ticket["company"], bus_ticket["departure"], bus_ticket["destination"], bus_ticket["date"], bus_ticket["time"], bus_ticket["fee"], bus_ticket["seats"])
     bus_tickets.append(bus_tick_obj)
 
-tren_json = requests.get("https://api.npoint.io/6d3c5a8f8064dfbd6be3").json()
+#tren data without compartments and seats https://api.npoint.io/6d3c5a8f8064dfbd6be3
+tren_json = requests.get("https://api.npoint.io/6bf62bd3840119596101").json()
 train_tickets = []
 for train_ticket in tren_json:
-    train_tick_obj = Train(train_ticket["id"], train_ticket["company"], train_ticket["departure"], train_ticket["destination"], train_ticket["date"], train_ticket["time"], train_ticket["fee"])
+    train_tick_obj = Train(train_ticket["id"], train_ticket["company"], train_ticket["departure"], train_ticket["destination"], train_ticket["date"], train_ticket["time"], train_ticket["fee"], train_ticket["compartments"], train_ticket["seats"])
     train_tickets.append(train_tick_obj)
 
 app = Flask(__name__)
