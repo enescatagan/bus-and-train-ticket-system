6542805f108d6a58cf16dfbe8b616e4b1151ead6
Taha Ugurcan Ongel <tahagurcanoffical@gmail.com>
2023-05-29 17:51:19 +0300

    fix merge issues

--- bus.py
+++ bus.py
@@ -1,13 +1,3 @@
-<<<<<<< HEAD
-
-class Bus():
-    def __init__(self, name, departure, destination, date, time) -> None:
-      self.name = name
-      self.departure = departure  
-      self.destination = destination
-      self.date = date
-      self.time = time
-=======
 class Bus():
     """Bus Tickets"""
     def __init__(self, id, company, departure, destination, date, time, fee) -> None:
@@ -19,4 +9,3 @@ class Bus():
       self.time = time
       self.fee = fee
         
->>>>>>> b35ee7f030f1f9a17ca38ee0651cc15abd99a6a2
