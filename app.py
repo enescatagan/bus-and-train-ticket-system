b084db5f48a9a6e78be17cbcaf64b50997231dc9
enescatagan <enescatagan@gmail.com>
2023-05-29 17:07:15 +0300

    Flask bus / Tren

--- app.py
+++ app.py
@@ -1,11 +1,28 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 
 @app.route('/')
-@app.route('/index')
-def hello_world():
-    return 'Main Page'
+def home():
+    return render_template("index.html")
+
+
+@app.route('/otobus')
+def otobus():
+    return render_template("otobus.html")
+
+@app.route('/otobus_biletleri')
+def otobus_biletleri():
+    return render_template("otobus_biletleri.html")
+
+
+@app.route('/tren')
+def tren():
+    return render_template("tren.html")
+
+@app.route('/tren_biletleri')
+def tren_biletleri():
+    return render_template("tren_biletleri.html")
 
 
 if __name__ == "__main__":
